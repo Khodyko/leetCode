cd9443bf6a685e9d9a1636a03062199bd7a2f1ce
khodyko <idv1llka@gmail.com>
2026-02-08 22:58:37 +0300

    BalancedBinaryTree

---
+++ src/main/java/BalancedBinaryTree.java
@@ -0,0 +1,77 @@
+public class BalancedBinaryTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    //Given a binary tree, determine if it is height-balanced.
+    //
+    //
+    //
+    //Example 1:
+    //
+    //
+    //Input: root = [3,9,20,null,null,15,7]
+    //Output: true
+    //Example 2:
+    //
+    //
+    //Input: root = [1,2,2,3,3,null,null,4,4]
+    //Output: false
+    //Example 3:
+    //
+    //Input: root = []
+    //Output: true
+    //
+    //
+    //Constraints:
+    //
+    //The number of nodes in the tree is in the range [0, 5000].
+    //-104 <= Node.val <= 104
+
+
+    //    Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //https://leetcode.com/problems/balanced-binary-tree/?envType=daily-question&envId=2026-02-08
+    public static boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private static int checkHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = checkHeight(root.left);
+        if (left == -1) {
+            return -1;
+        }
+
+        int right = checkHeight(root.right);
+        if (right == -1) {
+            return -1;
+        }
+        if (Math.abs(left - right) > 1)
+            return -1;
+
+        return Math.max(left, right) + 1;
+    }
+}
