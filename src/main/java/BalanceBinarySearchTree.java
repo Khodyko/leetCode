696c7baf2153c12c0aec63308b4304b023e8be1f
khodyko <idv1llka@gmail.com>
2026-02-09 23:15:15 +0300

    BalanceBinarySearchTree начало

---
+++ src/main/java/BalanceBinarySearchTree.java
@@ -0,0 +1,26 @@
+public class BalanceBinarySearchTree {
+    public static void main(String[] args) {
+
+    }
+
+
+//   Definition for a binary tree node.
+   public static class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+
+    public static TreeNode balanceBST(TreeNode root) {
+
+    }
+
+
+}
