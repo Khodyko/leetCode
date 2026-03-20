5ed7bde2688f17938a6233f656ed84f4a5334518
khodyko <idv1llka@gmail.com>
2026-03-20 23:44:02 +0300

    SymmetricTree

---
+++ src/main/java/SymmetricTree.java
@@ -0,0 +1,28 @@
+public class SymmetricTree {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+   public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+
+
+    public boolean isSymmetric(TreeNode root) {
+
+
+        return false;
+    }
+}
