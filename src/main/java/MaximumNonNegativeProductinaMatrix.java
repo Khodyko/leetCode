6035c879a92dfa88d3a22ceda9a95173597253de
khodyko <idv1llka@gmail.com>
2026-03-23 23:26:50 +0300

    MaximumNonNegativeProductinaMatrix

---
+++ src/main/java/MaximumNonNegativeProductinaMatrix.java
@@ -0,0 +1,10 @@
+public class MaximumNonNegativeProductinaMatrix {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int maxProductPath(int[][] grid) {
+       return 0;
+    }
+}
