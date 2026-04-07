ff4cc05db81d1602521c82f7023cec6e6cfeff3c
Игорь Ходыко <idv1llka@gmail.com>
2026-04-07 22:31:57 +0300

    Update SQLs.sql

--- src/main/java/SQLs.sql
+++ src/main/java/SQLs.sql
@@ -2,3 +2,19 @@
 SELECT s.score,
            DENSE_RANK() OVER (ORDER BY s.score DESC) AS rank
     FROM Scores s;
+
+-- Продолжаю постигать оконные функции
+-- LAG позволяет увидеть прошлые значения,
+-- LEAD позволяет увидеть значения впереди
+-- https://leetcode.com/problems/consecutive-numbers/
+
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT
+        num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+        LEAD(num, 1) OVER (ORDER BY id) AS next_num
+    FROM logs
+) sub
+WHERE num = prev_num AND num = next_num;
