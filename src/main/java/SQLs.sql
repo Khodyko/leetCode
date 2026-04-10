1ccac1592564070169331a2770e3ebc75d2fe63e
Игорь Ходыко <idv1llka@gmail.com>
2026-04-10 23:05:07 +0300

    Update SQLs.sql

--- src/main/java/SQLs.sql
+++ src/main/java/SQLs.sql
@@ -42,3 +42,27 @@ SELECT DISTINCT Email from
         FROM Person p
     )
     WHERE c>=2
+
+
+
+-- https://leetcode.com/problems/department-highest-salary/
+
+-- Простое решение:
+SELECT d.name as Department, e.name as Employee, e.salary as Salary
+FROM Employee e JOIN Department d ON e.departmentId=d.id
+WHERE e.salary=(SELECT MAX(salary) FROM Employee WHERE departmentId = e.departmentId);
+
+-- Ну и наши любимые оконные функции (есть ощущение, что я начинаю понимать как это работает):
+
+SELECT
+      a.d as Department,
+      a.e as Employee,
+      a.s as Salary
+FROM (
+    SELECT dep.name as d, emp.name as e, emp.salary as s,
+    DENSE_RANK() OVER (PARTITION BY emp.departmentId ORDER by emp.salary DESC)
+    as rnk
+    FROM Employee emp
+    JOIN Department dep ON dep.id=emp.departmentId
+) as a
+WHERE a.rnk=1;
