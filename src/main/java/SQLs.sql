b0b775c11c80750ec1a3dd03d05fe7be0c7c87e8
Игорь Ходыко <idv1llka@gmail.com>
2026-04-22 22:21:22 +0300

    Update SQLs.sql

--- src/main/java/SQLs.sql
+++ src/main/java/SQLs.sql
@@ -221,3 +221,26 @@ WHERE area>=3000000 OR population>=25000000
 SELECT class FROM Courses
 GROUP BY class
 HAVING COUNT(student)>=5
+
+
+
+
+
+-- Вот сегодняшняя задачка по бд
+-- https://leetcode.com/problems/human-traffic-of-stadium/
+
+
+
+-- Write your PostgreSQL query statement below
+SELECT id, visit_date, people FROM
+(
+    SELECT id, visit_date, people, 
+     LAG(people,1) OVER (ORDER BY id)  AS prev1,
+        LAG(people,2) OVER (ORDER BY id)  AS prev2,
+        LEAD(people,1) OVER (ORDER BY id) AS next1,
+        LEAD(people,2) OVER (ORDER BY id) AS next2
+    FROM Stadium
+) as s
+WHERE   (people >= 100 AND prev1 >= 100 AND prev2 >= 100)
+ OR (people >= 100 AND prev1 >= 100 AND next1 >= 100)
+ OR (people >= 100 AND next1 >= 100 AND next2 >= 100)
